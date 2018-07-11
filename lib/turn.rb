e2731ac37fe856ea43cc2699dedc44cc59aded89
Roberto Garza <robertg4567@hotmail.com>
2018-07-11 14:18:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,9 @@ def move(board, index, token = "X")
 end
 
 def turn(board)
-
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
 
 
   while !valid_move?(board, index) do
